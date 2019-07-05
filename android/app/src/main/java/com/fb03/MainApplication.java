a493ae0eda79be6f9b170c4aca60837f5041d002
Viet Anh Tran <anh.tran@medihub.com.vn>
2019-07-05 16:18:53 +0700

    1 . react-native link react-native-google-signin 2 . Update android/build.gradle: googlePlayServicesAuthVersion = "15.0.1"

--- android/app/src/main/java/com/fb03/MainApplication.java
+++ android/app/src/main/java/com/fb03/MainApplication.java
@@ -3,6 +3,7 @@ package com.fb03;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
 //            new FBSDKPackage(),
           new FBSDKPackage(mCallbackManager)
       );

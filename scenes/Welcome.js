7c31f94a4f946a3a2dc9e892e8ea06e5cfb5bff2
Viet Anh Tran <anh.tran@medihub.com.vn>
2021-04-01 16:13:29 +0700

    [package] [pod] - Cài react-native-image-crop-picker 0.19.1

--- scenes/Welcome.js
+++ scenes/Welcome.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity, Alert, BackHandler } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { NavigationEvents } from 'react-navigation';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -12,7 +13,6 @@ const instructions = Platform.select({
 
 export default class Welcome extends Component {
   componentDidMount() {
-    console.view.log('thang Kun la mot thang ngu!');
     
   }
 
@@ -36,6 +36,40 @@ export default class Welcome extends Component {
     Actions.Main();
   };
 
+  onUpload = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 300,
+        height: 300,
+        cropping: false,
+        includeBase64: true,
+        includeExif: true,
+      });
+      console.view.log(image);
+      const uri = Platform.OS == 'ios' ? image.sourceURL.replace("file://", '') : image.path;
+
+      // // Upload using rn fetch blob
+      // const elements = [
+      //   { name: '', filename: image.filename || uri.replace(/^.*[\\\/]/, ''), type: image.mime, data: image.data },
+      // ];
+      // console.view.log('uploadDegree2...');
+
+      // this.props.uploadDegree2(elements, this.formik.setFieldValue);
+    }
+    catch (err) {
+      if (err.code == 'E_PERMISSION_MISSING') {
+        alert("Ứng dụng không được cấp quyền vào thư viện hình ảnh. Bạn hãy vào phần Tuỳ chỉnh của thiết bị để cấp lại quyền truy cập.");
+      }
+      else if (err.code == 'E_PICKER_CANCELLED') {
+        //@todo
+      }
+      else {
+        console.view.log(err);
+        alert("Không upload được hình ảnh. Bạn có thể tạm BỎ QUA bước này. Nhân viên tư vấn sẽ gọi xác nhận nhà thuốc sau");
+      }
+    }
+  };
+
   onDidFocus = () => {
     BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
   };
@@ -50,6 +84,10 @@ export default class Welcome extends Component {
         <NavigationEvents onDidFocus={this.onDidFocus} onWillBlur={this.onWillBlur} />
         <TouchableOpacity style={styles.view} onPress={this.onMain}>
           <Text style={styles.welcome}>Welcome to React Native!</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.view} onPress={this.onUpload}>
+          <Text style={styles.welcome}>Upload File...</Text>
         </TouchableOpacity>
 
         <Text style={styles.instructions}>To get started, edit App.js</Text>
